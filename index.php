9730af77a3a79912ea801aff918a5429ac77676f
TakeMyTrinity <malingelilian49@gmail.com>
2022-10-14 16:33:40 +0200

    add Utils and Vues

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// inclusion en une seule fois du fichier des fonctions et du modèle
+require_once("utils/class.pdoPuyDuFou.inc.php");
+require_once("utils/functions.inc.php");
+// inclusion des vues footer et header
+include("Vues/v_footer.php");
+include("Vues/v_header.php");
+
+
+if (!isset($_REQUEST['uc']))
+    $uc = 'accueil';
+else
+    $uc = $_REQUEST['uc'];
+
+// On fait un appel à l'instance de la class PdoPuyDuFou
+$pdo = PdoPuyDuFou::getPdoPuyDuFou();
+
+//Switch pour récuperer la valeur du uc dans l'url
+switch ($uc) {
+    case 'accueil': {
+            include("Vues/v_accueil.php");
+            break;
+        }
+}
